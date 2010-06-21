8a6d5bfef61aa3cb761b2923e2e6ff0ddddd38ef
Ken Miller <ken@corp.mamasource.com>
2010-06-20 21:12:26 -0700

    erlang single-threaded complete

---
+++ erlang/tic_tac_toe.erl
@@ -0,0 +1,91 @@
+
+-module(tic_tac_toe).
+-export([main/0]).
+
+%% Move-Making
+place(Head, Space, {continue, CurrentPlayer, [Space|Rest], NextPlayer}) ->
+	Board = Head ++ [CurrentPlayer|Rest],
+	{result(Board), NextPlayer, Board, CurrentPlayer};
+
+place(Head, Space, {continue, CurrentPlayer, [X|Rest], NextPlayer}) ->
+	place(Head ++ [X], Space, {continue, CurrentPlayer, Rest, NextPlayer});
+
+place(Head, _Space, {continue, CurrentPlayer, [], NextPlayer}) ->
+	{continue, CurrentPlayer, Head, NextPlayer}.
+
+place(Space, GameState) ->
+	place([], Space, GameState).
+
+%% Result-Checking
+result([P, P, P, 
+		_, _, _, 
+		_, _, _]) -> win;
+
+result([_, _, _, 
+		P, P, P, 
+		_, _, _]) -> win;
+
+result([_, _, _, 
+		_, _, _, 
+		P, P, P]) -> win;
+
+result([P, _, _, 
+		P, _, _, 
+		P, _, _]) -> win;
+
+result([_, P, _, 
+		_, P, _, 
+		_, P, _]) -> win;
+
+result([_, _, P, 
+		_, _, P, 
+		_, _, P]) -> win;
+
+result([P, _, _, 
+		_, P, _, 
+		_, _, P]) -> win;
+
+result([_, _, P, 
+		_, P, _, 
+		P, _, _]) -> win;
+
+result([H|_]) when is_integer(H) -> continue;
+result([_|T])                    -> result(T);
+result([])                       -> tie.
+
+%% Output
+fmt() -> "\n~s|~s|~s\n---+---+---\n~s|~s|~s\n---+---+---\n~s|~s|~s\n\n".
+
+cell_repr(X) when is_integer(X) -> io_lib:format("(~p)",[X]);
+cell_repr(X)                    -> io_lib:format(" ~s ",[X]).
+
+print(Xs) 	            -> io:format(fmt(), [cell_repr(X) || X <- Xs]).
+
+check_status({continue, CurP, Board, NextP}) ->
+	Prompt = io_lib:format("Select a square, ~s: ", [CurP]),
+	Input = io:get_line(Prompt),
+	{Space, _} = string:to_integer(Input),
+	place(Space, {continue, CurP, Board, NextP});
+
+check_status({tie, _, _, _}) ->
+	io:format("It's a Draw!\n"),
+	quit;
+
+check_status({win, _, _, Winner}) ->
+	io:format("~s Wins!\n", [Winner]),
+	quit.
+
+%% Game Loop
+
+play(quit) -> init:stop();
+
+play(GameState) ->
+	{_, _, Board, _} = GameState,
+	print(Board),
+	play(check_status(GameState)).
+
+main() ->
+	play({continue, "X", [1,2,3,4,5,6,7,8,9], "O"}).
+
+
+
