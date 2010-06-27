c5406bbc1cd44e741df1653650c24afc0c9395d1
Ken Miller <ken@corp.mamasource.com>
2010-06-27 12:50:31 -0700

    mostly works, minus the fact that jsc has no way to output on the command line without a newline!

--- javascript/tic-tac-toe.js
+++ javascript/tic-tac-toe.js
@@ -9,8 +9,8 @@ if (typeof Object.beget !== 'function') {
     };
 }
 
-x = { toString: function() { return " X "; } }
-o = { toString: function() { return " O "; } }
+x = { plain: "X", toString: function() { return " X "; } }
+o = { plain: "O", toString: function() { return " O "; } }
 x.other = o;
 o.other = x;
 
@@ -45,10 +45,58 @@ makeBoard = function () {
 				this.spaces.slice(4,7).join(colSep),
 				this.spaces.slice(7,10).join(colSep)
 			].join(rowSep)
+		},
+		full: function () {
+			for (var i = 1; i <= 9; i++) {
+				if (this.spaces[i].vacant) {
+					return false;
+				}
+			}
+			return true;
+		},
+		match3: function (x,y,z) {
+			sp = this.spaces;
+			if (sp[x] === sp[y] && sp[y] === sp[z]) {
+				return sp[x];
+			} else {
+				return false;
+			}
+		},
+		winner: function () {
+			/* Taking advantage of short-circuit evaluation to
+			 * bubble up the winner. */
+			return this.match3(1,2,3) ||
+				this.match3(4,5,6) ||
+				this.match3(7,8,9) ||
+				this.match3(1,4,7) ||
+				this.match3(2,5,8) ||
+				this.match3(3,6,9) ||
+				this.match3(1,5,9) ||
+				this.match3(3,5,7);
 		}
 	}
 }
 
 board = makeBoard();
+player = x;
 
-print(board);
+while (true) {
+	print();
+	print(board);
+	print();
+	maybe_winner = board.winner();
+	if (maybe_winner) {
+		print(maybe_winner.plain + " Wins!");
+		break;
+	} else if (board.full()) {
+		print("It's a Draw!");
+		break;
+	} else {
+		print("Select a square, " + player.plain + ":");
+		space = parseInt(readline());
+		if (board.place(space, player)) {
+			player = player.other;
+		}
+	}
+}
+
